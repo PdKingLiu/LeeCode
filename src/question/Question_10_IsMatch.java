7cb0baaf03ce48326f6c4a3e11515ebc4dc9dfa7
PdKingLiu <931942280@qq.com>
2019-04-15 10:58:36 +0800

    maxArea

---
+++ src/question/Question_10_IsMatch.java
@@ -0,0 +1,8 @@
+package question;
+
+/**
+ * @author liupeidong
+ * Created on 2019/4/15 10:25
+ */
+public class Question_10_IsMatch {
+}
---
+++ src/question/Question_11_MaxArea.java
@@ -0,0 +1,37 @@
+package question;
+
+/**
+ * @author liupeidong
+ * Created on 2019/4/15 10:45
+ */
+public class Question_11_MaxArea {
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+        System.out.println(maxAreaVioLence(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+
+    public static int maxArea(int[] height) {
+        int maxArea = 0;
+        int l = 0;
+        int r = height.length - 1;
+        while (l < r) {
+            maxArea = Math.max(maxArea, Math.min(height[l], height[r]) * (r - l));
+            if (height[r] < height[l]) {
+                r--;
+            } else {
+                l++;
+            }
+        }
+        return maxArea;
+    }
+
+    public static int maxAreaVioLence(int[] height) {
+        int maxArea = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * (j - i));
+            }
+        }
+        return maxArea;
+    }
+}
