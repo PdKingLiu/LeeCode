4958894d7df3f5d25b51090220aa09108f16cdd0
PdKingLiu <931942280@qq.com>
2019-04-25 23:54:25 +0800

    mergeTwoLists

---
+++ src/question/Question_21_mergeTwoLists.java
@@ -0,0 +1,28 @@
+package question;
+
+import common.ListNode;
+
+/**
+ * @author liupeidong
+ * Created on 2019/4/25 23:48
+ */
+public class Question_21_mergeTwoLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode head = null;
+        if (l1.val < l2.val) {
+            head = l1;
+            head.next = mergeTwoLists(l1.next, l2);
+        } else {
+            head = l2;
+            head.next = mergeTwoLists(l1, l2.next);
+        }
+        return head;
+    }
+}
--- src/question/Test.java
+++ src/question/Test.java
@@ -1,10 +1,16 @@
 package question;
 
+import java.util.HashMap;
+
 /**
  * @author liupeidong
  * Created on 2019/4/9 23:14
  */
 public class Test {
+
+    public static int[] longs = new int[50];
+
     public static void main(String[] args) {
     }
+
 }
