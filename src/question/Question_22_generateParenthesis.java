6d9badc9c96b6bc64693a0bbfe39123eff198c84
PdKingLiu <931942280@qq.com>
2019-04-28 17:40:20 +0800

    括号生成之回溯

---
+++ src/question/Question_22_generateParenthesis.java
@@ -0,0 +1,34 @@
+package question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author liupeidong
+ * Created on 2019/4/28 17:15
+ */
+public class Question_22_generateParenthesis {
+    public static void main(String[] args) {
+        System.out.println(generateParenthesis(3));
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+
+    public static void backtrack(List<String> list, String str, int i, int i1, int n) {
+        if (str.length() == n * 2) {
+            list.add(str);
+            return;
+        }
+        if (i < n) {
+            backtrack(list, str + "(", i + 1, i1, n);
+        }
+        if (i1 < i) {
+            backtrack(list, str + ")", i, i1 + 1, n);
+        }
+    }
+
+}
