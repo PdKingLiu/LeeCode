949c41a82047aeac0219d3ad189771b4862dfcb2
PdKingLiu <931942280@qq.com>
2019-07-30 19:40:18 +0800

    Question_62_uniquePaths

---
+++ src/question/Question_62_uniquePaths.java
@@ -0,0 +1,40 @@
+package question;
+
+/**
+ * @author liupeidong
+ * Created on 2019/7/30 19:16
+ */
+public class Question_62_uniquePaths {
+
+/*
+    一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+
+    机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+
+    问总共有多少条不同的路径？
+
+    说明：m 和 n 的值均不超过 100。
+
+    示例 1:
+
+    输入: m = 3, n = 2
+    输出: 3
+    解释:
+    从左上角开始，总共有 3 条路径可以到达右下角。
+    1. 向右 -> 向右 -> 向下
+    2. 向右 -> 向下 -> 向右
+    3. 向下 -> 向右 -> 向右
+
+*/
+
+    public static int uniquePaths(int m, int n) {
+        int dp[][] = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = i == 0 || j == 0 ? 1 : dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+
+}
