68177227b360c1ff389631f01dba4b6308fa77a6
PdKingLiu <931942280@qq.com>
2019-08-02 10:55:44 +0800

    Question_40_combinationSum2 回溯

--- src/question/Question_40_combinationSum2.java
+++ src/question/Question_40_combinationSum2.java
@@ -6,37 +6,30 @@ import java.util.List;
 
 /**
  * @author liupeidong
- * Created on 2019/8/2 9:35
+ * Created on 2019/8/2 10:17
  */
-public class Question_39_combinationSum {
+public class Question_40_combinationSum2 {
 
-    /*给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+    /*给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
 
-        candidates 中的数字可以无限制重复被选取。
+        candidates 中的每个数字在每个组合中只能使用一次。
 
         说明：
 
-        所有数字（包括 target）都是正整数。
+        所有数字（包括目标数）都是正整数。
         解集不能包含重复的组合。 
         示例 1:
 
-        输入: candidates = [2,3,6,7], target = 7,
+        输入: candidates = [10,1,2,7,6,1,5], target = 8,
         所求解集为:
         [
-          [7],
-          [2,2,3]
-        ]
-        示例 2:
-
-        输入: candidates = [2,3,5], target = 8,
-        所求解集为:
-        [
-          [2,2,2,2],
-          [2,3,3],
-          [3,5]
+          [1, 7],
+          [1, 2, 5],
+          [2, 6],
+          [1, 1, 6]
         ] */
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> lists = new ArrayList<>();
         backtrack(target, 0, candidates, new ArrayList<>(), lists);
@@ -52,10 +45,12 @@ public class Question_39_combinationSum {
             return;
         }
         for (int i = start; i < candidates.length && target - candidates[i] >= 0; i++) {
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
             list.add(candidates[i]);
-            backtrack(target - candidates[i], i, candidates, list, lists);
+            backtrack(target - candidates[i], i + 1, candidates, list, lists);
             list.remove(list.size() - 1);
         }
     }
-
 }
