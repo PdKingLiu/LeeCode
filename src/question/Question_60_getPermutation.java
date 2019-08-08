b92be38b697d98ae07bff4d1fcc6a41d879e5e29
PdKingLiu <931942280@qq.com>
2019-08-08 17:48:13 +0800

    第k个排列 借用阶乘 复杂度O(n)

--- src/question/Question_60_getPermutation.java
+++ src/question/Question_60_getPermutation.java
@@ -32,6 +32,52 @@ public class Question_60_getPermutation {
         输入: n = 3, k = 3
         输出: "213" */
 
+
+    /*线性遍历*/
+
+    public String getPermutation2(int n, int k) {
+        List<Integer> list = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(i + 1);
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        int[] J = new int[n + 1];
+        getJ(J, n);
+        for (int i = 0; k > 0; i++) {
+            if (k == 1) {
+                for (Integer in : list) {
+                    stringBuilder.append(in);
+                }
+                break;
+            }
+            int a = k / J[n - i - 1];
+            if (a == 0) {
+                stringBuilder.append(list.get(0));
+                list.remove(0);
+            } else if (J[n - i - 1] * a == k) {
+                stringBuilder.append(list.get(a - 1));
+                list.remove(a - 1);
+                if (a > 1) {
+                    k = k - (a - 1) * J[n - i - 1];
+                }
+            } else {
+                stringBuilder.append(list.get(a));
+                list.remove(a);
+                k = k - J[n - i - 1] * a;
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private void getJ(int[] j, int n) {
+        j[0] = 0;
+        j[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            j[i] = j[i - 1] * i;
+        }
+    }
+
+
     /*回溯超时*/
 
     List<String> list = new LinkedList<>();
