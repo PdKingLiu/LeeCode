698abae776a489b45d3bade5c9ce6be1bba49366
PdKingLiu <931942280@qq.com>
2019-08-24 21:28:58 +0800

    145. 二叉树的后序遍历 栈

--- src/question/Question_145_postorderTraversal.java
+++ src/question/Question_145_postorderTraversal.java
@@ -2,6 +2,7 @@ package question;
 
 import common.TreeNode;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -25,7 +26,7 @@ public class Question_145_postorderTraversal {
         输出: [3,2,1]
         进阶: 递归算法很简单，你可以通过迭代算法完成吗？ */
 
-    List<Integer> list = new LinkedList<>();
+    LinkedList<Integer> list = new LinkedList<>();
 
     /*递归*/
 
@@ -38,4 +39,20 @@ public class Question_145_postorderTraversal {
         list.add(root.val);
         return list;
     }
+
+    /*非递归 栈*/
+
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                list.addFirst(root.val);
+                stack.push(root);
+                root = root.right;
+            }
+            root = stack.pop().left;
+        }
+        return list;
+    }
+
 }
