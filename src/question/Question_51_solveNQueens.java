8f10cbf8b79735967ddc489d687fe6ca731dd315
PdKingLiu <931942280@qq.com>
2019-08-28 18:00:33 +0800

    51. N皇后 剪枝回溯

--- src/question/Question_51_solveNQueens.java
+++ src/question/Question_51_solveNQueens.java
@@ -111,4 +111,76 @@ public class Question_51_solveNQueens {
         return true;
     }
 
+//    -------------------------------------------------------------------
+
+    /*设置标记*/
+
+    boolean[] h;
+    boolean[] s;
+    boolean[] xxia;
+    boolean[] xshang;
+
+    public List<List<String>> solveNQueens2(int n) {
+        indexs = new char[n][n];
+        h = new boolean[n];
+        s = new boolean[n];
+        xxia = new boolean[2 * n - 1];
+        xshang = new boolean[2 * n - 1];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(indexs[i], '.');
+        }
+        backtrack2(0, n);
+        return lists;
+    }
+
+    private void backtrack2(int i, int n) {
+        if (i == n) {
+            lists.add(result(indexs));
+            return;
+        }
+        for (int j = 0; j < n; j++) {
+            if (judge(i, j, n)) {
+                indexs[i][j] = 'Q';
+                changeFlag(i, j, n, true);
+                backtrack2(i + 1, n);
+                indexs[i][j] = '.';
+                changeFlag(i, j, n, false);
+            }
+        }
+    }
+
+    private void changeFlag(int i, int j, int n, boolean b) {
+        h[i] = b;
+        s[j] = b;
+        xshang[i + j] = b;
+        if (i >= j) {
+            xxia[i - j] = b;
+        } else {
+            xxia[j - i + n - 1] = b;
+        }
+    }
+
+    private boolean judge(int i, int j, int n) {
+        boolean[] booleans = {false, false, false, false};
+        if (!h[i]) {
+            booleans[0] = true;
+        }
+        if (!s[j]) {
+            booleans[1] = true;
+        }
+        if (i >= j) {
+            if (!xxia[i - j]) {
+                booleans[2] = true;
+            }
+        } else {
+            if (!xxia[j - i + n - 1]) {
+                booleans[2] = true;
+            }
+        }
+        if (!xshang[i + j]) {
+            booleans[3] = true;
+        }
+        return booleans[0] && booleans[1] && booleans[2] && booleans[3];
+    }
+
 }
